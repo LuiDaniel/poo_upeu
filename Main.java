931219438724ac7635977e53e3667b9bbfe9d44e
Luis Daniel <lui.daniel.dev@gmail.com>
2026-08-13 14:48:42 -0500

    Vector de meses

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
+    public static void meses(){
+        String[] mes = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        System.out.println("Tamaño: " + mes.length);
+        
+        for(String m : mes){
+            System.out.println(m);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        meses();
     }
 }
